31770e74d4a0f40ad9421f530175a924fe52b494
DReX97 <148553511+DReX97@users.noreply.github.com>
2025-10-30 15:49:27 +0000

    Added main.py starter file - initial setup

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to Historical Cipher Group I!")
+    print("This program will include Rail Fence, Caesar, and Vigenere ciphers.")
+
+if __name__ == "__main__":
+    main()
