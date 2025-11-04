f0328de6b3dda0873714c70209c84f74fc1dfc73
rossp2005 <rossprizeman@gmail.com>
2025-11-04 10:10:06 +0000

    first changes to original atbash
    -improved dictionary lookup to a easier and safer transform
    - not dependent on input being all uppercase characters
    - can use both upper and lower case characters
    - still uses the hardcoded example doesnt have a menu

--- atbash.py
+++ atbash.py
@@ -1,47 +1,24 @@
-# Pyhton program to implement Atbash Cipher
+"""
+origninal link :https://github.com/palash25/pyphers/blob/master/atbashCipher.py
+accessed on the 4/11/2025
+edited by Ross Prizeman
 
-# Its one of the simplest ciphers with almost no security
-
-'''
-Every character is reversed i.e. 'A' changes to 'Z'
-and vice versa.
-*-----------------------------------------------------*
-* A B C D E F G H I J K L M N O P Q R S T U V W X Y Z *
-* | | | | | | | | | | | | | | | | | | | | | | | | | | *
-* | | | | | | | | | | | | | | | | | | | | | | | | | | *
-* V V V V V V V V V V V V V V V V V V V V V V V V V V *
-* Z Y X W V U T S R Q P O N M L K J I H G F E D C B A *
-*-----------------------------------------------------*
-It can be thought of as an affine cipher
-with a = 25 = b
-
-'''
-# This script uses dictionaries to lookup various alphabets
-lookup_table = {'A': 'Z', 'B': 'Y', 'C': 'X', 'D': 'W', 'E': 'V',
-                'F': 'U', 'G': 'T', 'H': 'S', 'I': 'R', 'J': 'Q',
-                'K': 'P', 'L': 'O', 'M': 'N', 'N': 'M', 'O': 'L',
-                'P': 'K', 'Q': 'J', 'R': 'I', 'S': 'H', 'T': 'G',
-                'U': 'F', 'V': 'E', 'W': 'D', 'X': 'C', 'Y': 'B', 'Z': 'A'}
-
-'''
-Since we are using a dictionary we don't need
-separate functions for encryption and decryption
-'''
-
-
-def atbash(message):
-    cipher = ''
-    for letter in message:
-        # checks for space
-        if(letter != ' '):
-            # adds the corresponding letter from the lookup_table
-            cipher += lookup_table[letter]
+changes made :
+- improved from a dictionary lookup to a easier and safer transform
+- not dependent on input being all uppercase characters
+- can use both upper and lower case characters
+- still uses the hardcoded example
+"""
+def atbash(text):
+    result = ""
+    for ch in text:
+        if 'A' <= ch <= 'Z':
+            result += chr(ord('Z') - (ord(ch) - ord('A')))
+        elif 'a' <= ch <= 'z':
+            result += chr(ord('z') - (ord(ch) - ord('a')))
         else:
-            # adds space
-            cipher += ' '
-
-    return cipher
-
+            result += ch
+    return result
 # Hard-coded driver function to run the program
 
 
